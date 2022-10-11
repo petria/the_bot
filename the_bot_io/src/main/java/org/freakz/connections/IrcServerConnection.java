eb667e08dc03b51b6a6454e8e84484e857d7f584
Petri Airio <petri.j.airio@gmail.com>
2022-10-12 00:14:25 +0300

    - remove try / catch

--- the_bot_io/src/main/java/org/freakz/connections/IrcServerConnection.java
+++ the_bot_io/src/main/java/org/freakz/connections/IrcServerConnection.java
@@ -6,7 +6,6 @@ import org.kitteh.irc.client.library.Client;
 import org.kitteh.irc.client.library.event.channel.ChannelJoinEvent;
 import org.kitteh.irc.client.library.event.channel.ChannelMessageEvent;
 import org.kitteh.irc.client.library.event.connection.ClientConnectionEstablishedEvent;
-import org.kitteh.irc.client.library.exception.KittehNagException;
 
 @Slf4j
 public class IrcServerConnection {
@@ -36,23 +35,20 @@ public class IrcServerConnection {
     }
 
     public void init() {
-        try {
-            Client client
-                    = Client.builder()
-                    .user("hokan")
-                    .nick("HokanDEV")
-                    .server()
-                    .host("irc.stealth.net")
-                    .port(6667, Client.Builder.Server.SecurityType.INSECURE)
-                    .then()
-                    .buildAndConnect();
 
-            client.getEventManager().registerEventListener(new Listener());
-            client.addChannel("#HokanDEV");
+        Client client
+                = Client.builder()
+                .user("hokan")
+                .nick("HokanDEV")
+                .server()
+                .host("irc.stealth.net")
+                .port(6667, Client.Builder.Server.SecurityType.INSECURE)
+                .then()
+                .buildAndConnect();
 
-        } catch (KittehNagException nag) {
-            log.debug("got nagged!");
-        }
+        client.getEventManager().registerEventListener(new Listener());
+        client.addChannel("#HokanDEV");
+
     }
 
 }
