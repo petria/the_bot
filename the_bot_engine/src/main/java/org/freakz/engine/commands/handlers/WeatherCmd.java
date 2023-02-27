1d2171f6fed186ee0cbbcc98c55445f4491bb6f1
Petri Airio <petri.j.airio@gmail.com>
2023-02-27 09:43:40 +0200

    - Show weather data

--- the_bot_engine/src/main/java/org/freakz/engine/commands/handlers/WeatherCmd.java
+++ the_bot_engine/src/main/java/org/freakz/engine/commands/handlers/WeatherCmd.java
@@ -5,7 +5,9 @@ import com.martiansoftware.jsap.JSAPException;
 import com.martiansoftware.jsap.UnflaggedOption;
 import lombok.extern.slf4j.Slf4j;
 import org.freakz.common.model.json.engine.EngineRequest;
+import org.freakz.common.util.StringStuff;
 import org.freakz.dto.KelikameratResponse;
+import org.freakz.dto.KelikameratWeatherData;
 import org.freakz.engine.commands.HokanCommandHandler;
 import org.freakz.engine.commands.api.AbstractCmd;
 import org.freakz.services.ServiceRequestType;
@@ -31,12 +33,55 @@ public class WeatherCmd extends AbstractCmd {
 
     }
 
+    private String formatWeather(KelikameratWeatherData d, boolean verbose) {
+        String template = "%s: %2.1f°C";
+        String placeFromUrl = d.getPlaceFromUrl();
+        placeFromUrl = placeFromUrl.substring(placeFromUrl.indexOf("_") + 1).replaceAll("_", " ");
+        String ret = String.format(template, placeFromUrl, d.getAir(), d.getRoad(), d.getGround());
+        if (verbose) {
+            ret += " [" + d.getUrl() + "]";
+        }
+        return ret;
+    }
+
     @Override
     public String executeCommand(EngineRequest engineRequest) {
 
+//        boolean verbose = results.getBoolean(ARG_VERBOSE);
+        boolean verbose = false;
+
+
         KelikameratResponse data = doServiceRequest(engineRequest, ServiceRequestType.KelikameratService);
         if (data.getStatus().startsWith("OK")) {
-            return data.getStatus();
+            StringBuilder sb = new StringBuilder();
+            int xx = 0;
+            String place = "";
+            String regexp = ".*" + place + ".*";
+
+            for (KelikameratWeatherData wd : data.getDataList()) {
+                String placeFromUrl = wd.getPlaceFromUrl();
+                String stationFromUrl = wd.getUrl().getStationUrl();
+                if (StringStuff.match(placeFromUrl, regexp) || StringStuff.match(stationFromUrl, regexp)) {
+                    if (wd.getAir() == null) {
+                        continue;
+                    }
+                    String formatted = formatWeather(wd, verbose);
+                    if (formatted.contains("Rantatunneli")) {
+                        continue;
+                    }
+
+                    if (xx != 0) {
+                        sb.append(", ");
+                    }
+                    sb.append(formatted);
+                    xx++;
+/*                    if (xx > results.getInt(ARG_COUNT)) {
+                        break;
+                    }*/
+                }
+            }
+            return sb.toString();
+//            return data.getStatus();
         } else {
             return this.getClass().getSimpleName() + " error :: " + data.getStatus();
         }
