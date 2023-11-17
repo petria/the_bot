4b1fcd99813dbaf3236cd57b0c77db7f8e2f78b9
Petri Airio <petri.j.airio@gmail.com>
2023-11-17 10:48:47 +0200

    Add Calculator command

---
+++ the_bot_engine/src/main/java/org/freakz/engine/commands/handlers/util/CalculatorCmd.java
@@ -0,0 +1,57 @@
+package org.freakz.engine.commands.handlers.util;
+
+import com.martiansoftware.jsap.JSAP;
+import com.martiansoftware.jsap.JSAPException;
+import com.martiansoftware.jsap.JSAPResult;
+import com.martiansoftware.jsap.UnflaggedOption;
+import lombok.extern.slf4j.Slf4j;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+import org.freakz.common.exception.NotImplementedException;
+import org.freakz.common.model.engine.EngineRequest;
+import org.freakz.engine.commands.HandlerAlias;
+import org.freakz.engine.commands.annotations.HokanCommandHandler;
+import org.freakz.engine.commands.api.AbstractCmd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.freakz.engine.commands.util.StaticArgumentStrings.ARG_EXPRESSION;
+
+@HokanCommandHandler
+@Slf4j
+public class CalculatorCmd extends AbstractCmd {
+
+    @Override
+    public void initCommandOptions(JSAP jsap) throws NotImplementedException, JSAPException {
+
+        jsap.setHelp("Simple calculator");
+
+        UnflaggedOption opt = new UnflaggedOption(ARG_EXPRESSION).setRequired(true).setGreedy(false);
+
+        jsap.registerParameter(opt);
+
+    }
+
+    @Override
+    public List<HandlerAlias> getAliases() {
+        List<HandlerAlias> list = new ArrayList<>();
+        list.add(createWithArgsAlias("!calc", "!calculator"));
+        return list;
+    }
+
+    @Override
+    public String executeCommand(EngineRequest request, JSAPResult results) {
+        String result;
+        try {
+            Expression expression = new ExpressionBuilder(results.getString(ARG_EXPRESSION)).build();
+            result = "" + expression.evaluate();
+
+        } catch (Exception e) {
+            result = e.getMessage();
+        }
+
+        return String.format("%s = %s", results.getString(ARG_EXPRESSION), result);
+    }
+
+}
