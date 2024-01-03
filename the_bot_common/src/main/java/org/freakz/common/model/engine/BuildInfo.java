d0666897a2ee56eacd9db9b8021e575a0f2963e8
Petri Airio <petri.j.airio@gmail.com>
2024-01-03 18:07:06 +0200

    Prepare to have build info

---
+++ the_bot_common/src/main/java/org/freakz/common/model/engine/BuildInfo.java
@@ -0,0 +1,26 @@
+package org.freakz.common.model.engine;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
+@ToString
+public class BuildInfo {
+
+    private String userName;
+    private String userTimezone;
+    private String javaVmVendor;
+    private String javaVmVersion;
+    private String javaVmName;
+    private String javaRuntimeVersion;
+    private String osName;
+    private String osVersion;
+    private String osArch;
+    private String javaHome;
+    private String buildTime;
+    private String gitRevision;
+    private String gitUrl;
+
+}
