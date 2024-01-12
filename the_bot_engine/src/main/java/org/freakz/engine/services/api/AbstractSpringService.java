3a35e9abc2a2384827f03deb714bca34f020a7b8
Petri Airio <petri.j.airio@gmail.com>
2024-01-12 08:28:03 +0200

    Show build info with more details

---
+++ the_bot_engine/src/main/java/org/freakz/engine/services/api/AbstractSpringService.java
@@ -0,0 +1,16 @@
+package org.freakz.engine.services.api;
+
+import lombok.Getter;
+import org.freakz.engine.config.ConfigService;
+
+public class AbstractSpringService extends AbstractService {
+
+    @Getter
+    private ConfigService configService;
+
+    @Override
+    public void initializeService(ConfigService configService) throws Exception {
+        this.configService = configService;
+    }
+
+}
--- the_bot_engine/src/main/java/org/freakz/engine/services/buildinfo/BuildInfoService.java
+++ the_bot_engine/src/main/java/org/freakz/engine/services/buildinfo/BuildInfoService.java
@@ -1,6 +1,5 @@
 package org.freakz.engine.services.buildinfo;
 
-import org.freakz.engine.config.ConfigService;
 import org.freakz.engine.services.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
@@ -8,24 +7,33 @@ import org.springframework.stereotype.Service;
 
 @Service
 @SpringServiceMethodHandler
-public class BuildInfoService extends AbstractService {
+public class BuildInfoService extends AbstractSpringService {
 
-    @Autowired
+    @Autowired(required = false)
     private BuildProperties bp;
-
-    @Override
-    public void initializeService(ConfigService configService) throws Exception {
-
-    }
 
     @ServiceMessageHandlerMethod(ServiceRequestType = ServiceRequestType.BuildInfoQuery)
     public <T extends ServiceResponse> ServiceResponse handleBuildInfoQuery(ServiceRequest request) {
+        String buildInfo;
+        if (bp != null) {
+            String details = String.format("java version: %s - os.arch: %s - os.name: %s - os.version: %s",
+                    bp.get("java.version"),
+                    bp.get("os.arch"),
+                    bp.get("os.name"),
+                    bp.get("os.version")
+            );
 
-        String buildInfo = String.format("BuildInfo: %s %s %s",
-                bp.getGroup(),
-                bp.getArtifact(),
-                bp.getVersion()
-        );
+            buildInfo = String.format("BuildInfo: %s %s %s - %s",
+                    bp.getGroup(),
+                    bp.getArtifact(),
+                    bp.getVersion(),
+                    details
+            );
+
+        } else {
+            buildInfo = "BuildInfo: n/a";
+        }
+
         ServiceResponse response = new ServiceResponse();
         response.setStatus(buildInfo);
         return response;
