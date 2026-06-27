e1b60bcd1691a2ec6ff497f823b2cfa80a33056a
Petri Airio <petri.j.airio@gmail.com>
2026-06-27 19:22:16 +0300

    Extend web async timeout for live streams

--- the_bot_web/src/main/java/org/freakz/web/config/SpaForwardConfig.java
+++ the_bot_web/src/main/java/org/freakz/web/config/SpaForwardConfig.java
@@ -1,11 +1,19 @@
 package org.freakz.web.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SpaForwardConfig implements WebMvcConfigurer {
+
+  private static final long ONE_HOUR_MILLIS = 60L * 60L * 1000L;
+
+  @Override
+  public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+    configurer.setDefaultTimeout(ONE_HOUR_MILLIS);
+  }
 
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
